77a13b6ad1e81f121d49fe68ed41af1ec4b6a127
Kristoffer <kristoffach@gmail.com>
2018-07-19 18:04:02 +0200

    initial commit
    first commit of project and ow subfolder

---
+++ owcompendium/assets/js/overwatch.js
@@ -0,0 +1,53 @@
+// Includes sub-pages //
+$(function() {
+    var $menu = $('#menu'),
+        $target = $('#content');
+
+    $menu.on('click', '> li a', function(event) {
+        var $this = $(this);
+        event.preventDefault();
+        $target.load($this.attr('href'));
+    });
+});
+
+$("#setfullscreen").click(function(){
+  $("#wrapper").toggleClass("fullscreen");
+},);
+
+// modal for map page //
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
